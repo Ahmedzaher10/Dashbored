0f922d1fa9c9de72ec8fd53bab325d8630ffee6f
Ahmedzaher10 <ahmedzaher.t7@gmail.com>
2024-09-18 00:29:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn  = document.querySelector("#close-btn");
+const themeToggler  = document.querySelector(".theme-toggler");
+
+
+// show sidebar
+menuBtn.addEventListener('click', ()=> {
+
+    sideMenu.style.display = 'block';
+
+});
+
+
+//close sidbar
+closeBtn.addEventListener('clik' , () => {
+
+    sideMenu.style.display = 'none';
+});
+
+
+// change theme 
+
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
+
+
